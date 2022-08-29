63cbbebe6edfc3327ab5faacc6c76722ffeb6e11
theco12 <107103821+theco12@users.noreply.github.com>
2022-08-29 17:29:13 +0900

    xptmxm

--- js/darkmode.js
+++ js/darkmode.js
@@ -4,3 +4,8 @@ $('.badge').on('click', function(){
     count = count +1;
     console.log(count);
 });
+
+const divtag = document.querySelector('div');
+
+divtag.innerHTML('안녕하세요.');
+divtag.style.fontSize ="30px";
