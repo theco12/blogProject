a70577ebc2f400249ec959aace965e96b8373fe8
theco12 <107103821+theco12@users.noreply.github.com>
2022-09-13 18:58:20 +0900

    react

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,12 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  let post = '강남우동맛집';
+  useState('남자 코트 추천')
+
   return (
     <div className="App">
-      <div class="blog_nav">
+      <div className="blog_nav">
         <h4>블로그임</h4>
       </div>
+      <div className="list">
+        <h4>글제목</h4>
+        <p>9월 11일 발행</p>
+      </div>
+      <div className="list">
+        <h4>글제목</h4>
+        <p>9월 11일 발행</p>
+      </div>
+      <div className="list">
+        <h4>글제목</h4>
+        <p>9월 11일 발행</p>
+      </div>
+      <div className="modal">
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+    
     </div>
   );
 }
