65cfe3d0374c5d46f7e02c0f76000a19cd73afe6
theco12 <107103821+theco12@users.noreply.github.com>
2022-11-23 19:06:44 +0900

    Update Shoes.js

--- blog/src/pages/Shoes.js
+++ blog/src/pages/Shoes.js
@@ -4,6 +4,14 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import data from "../data";
 import { useState } from "react";
+import styled from "styled-components";
+
+const Content = styled.div`
+  width: 100%;
+  max-width: 1440px;
+  height: 800px;
+  padding: 20px;
+`;
 
 const Shoes = () => {
   let [gift] = useState(data);
@@ -12,15 +20,25 @@ const Shoes = () => {
   return (
     <div>
       <div className="main-bg"></div>
-      <div>상품 페이지</div>
 
-      <Container>
-        <Row>
-          <Col>1 of 3</Col>
-          <Col>2 of 3</Col>
-          <Col>3 of 3</Col>
-        </Row>
-      </Container>
+      <Content>
+        <div>
+          어떻게 메타버스에서도 <br />
+          검증된 심리상담이 가능할까요?
+        </div>
+        <div>
+          메타포레스트는 멘탈 헬스, 심리상담 전문가가 <br />
+          철저한 검증을 거쳐 만들고 있습니다
+        </div>
+
+        <Container>
+          <Row>
+            <Col>1 of 3</Col>
+            <Col>2 of 3</Col>
+            <Col>3 of 3</Col>
+          </Row>
+        </Container>
+      </Content>
     </div>
   );
 };
