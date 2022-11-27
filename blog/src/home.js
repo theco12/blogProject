d5ccd8fae6c6d21a575b5345384706cd8a1df5ac
theco12 <107103821+theco12@users.noreply.github.com>
2022-11-27 17:23:42 +0900

    update

--- blog/src/home.js
+++ blog/src/home.js
@@ -1,4 +1,3 @@
-import styled from "styled-components";
 import React from "react";
 import { useRef } from "react";
 import Button from "./button.js";
--- blog/src/pages/Shoes.js
+++ blog/src/pages/Shoes.js
@@ -1,7 +1,4 @@
 import shoes from "../css/shoes.css";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import data from "../data.js";
 import { useState } from "react";
 import styled from "styled-components";
